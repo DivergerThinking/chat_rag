f76e2ed12282b0e570e4cd04a5aff40c5c7751dd
itor <itortouch@gmail.com>
2023-10-02 18:52:03 +0200

    Avoid default llm chain and pre-define api key

--- src/chatrag/retriever.py
+++ src/chatrag/retriever.py
@@ -1,18 +1,21 @@
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.retrievers.self_query.base import SelfQueryRetriever
-from langchain.chains.query_constructor.base import AttributeInfo
-from chatrag.csv_meta_loader import CSVMetaLoader
 from typing import Dict, Optional
+
+from langchain.chains import RetrievalQA
+from langchain.chains.query_constructor.schema import AttributeInfo
+from langchain.chat_models import ChatOpenAI
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.prompts import PromptTemplate
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+from chatrag.csv_meta_loader import CSVMetaLoader
+from chatrag.prompts import MOVIE_RETRIEVER_TEMPLATE
 
 
 def create_retriever_from_csv(
     csv_path: str,
+    llm: ChatOpenAI,
     metadata_columns_dtypes: Optional[Dict[str, str]] = {"monthly_traffic": "int"},
-    llm: ChatOpenAI = ChatOpenAI(temperature=0, model="gpt-4", max_tokens=2000),
 ):
     loader = CSVMetaLoader(csv_path, metadata_columns_dtypes=metadata_columns_dtypes)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=0)
@@ -42,28 +45,11 @@ def create_retriever_from_csv(
 
 
 def create_retrieval_chain(
-    retriever: SelfQueryRetriever, llm: ChatOpenAI = ChatOpenAI(temperature=0, model="gpt-3.5-turbo", max_tokens=2000)
+    retriever: SelfQueryRetriever,
+    llm: ChatOpenAI,
+    retriever_prompt_template: str = MOVIE_RETRIEVER_TEMPLATE,
 ):
-    template = """Use the following movies data to find the best matches for the user request in the question overview topic. Rules:
-    - You can return more than one movie if they are a good match.
-    - Answer with movie names of the medias and a short text justifying the choice.
-    - The justification must take into account overview topic matching and vote average score (higher=better).
-
-    Media data:
-    {context}
-
-    Question overview topic:
-    {question}
-
-    Example answer:
-    1. Title: [selected movie title]
-    - Justification: [Given justification]
-    - Score: [vote_average]
-
-    Movies attending to rules and ordered from best to worst:
-    """
-
-    retrieval_prompt = PromptTemplate(template=template, input_variables=["context", "question"])
+    retrieval_prompt = PromptTemplate(template=retriever_prompt_template, input_variables=["context", "question"])
 
     media_retriever_chain = RetrievalQA.from_chain_type(
         llm=llm,
