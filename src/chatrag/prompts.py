f156268cabcaae9a2639bb43f4533a38ab1a0d18
itor <itortouch@gmail.com>
2024-04-26 10:59:31 +0200

    Remove deprecated resources

--- src/chatrag/prompts.py
+++ src/chatrag/prompts.py
@@ -1,26 +1,3 @@
-MOVIE_RETRIEVER_TEMPLATE = """Use the following movies data to find the best matches for the user request in the question overview topic. Rules:
-- You can return more than one movie if they are a good match.
-- Answer with movie names of the medias and a short text justifying the choice.
-- The justification must take into account overview topic matching and vote average score (higher=better).
-
-Media data:
-{context}
-
-Question overview topic:
-{question}
-
-Example answer:
-1. Title: [selected movie title]
-- Justification: [Given justification]
-- Score: [vote_average]
-
-Movies attending to rules and ordered from best to worst:
-"""  # noqa
-
-REACT_PREFIX = """You are Diverger Assistant, an language model created by OpenAI and used by Diverger, an AI company. You are an assistant expert in movies recommendation.
-
-Take a deep breath, use the TOOLS when needed and ALWAYS follow RESPONSE FORMAT INSTRUCTIONS."""  # noqa
-
 MOVIE_CHATBOT_TEMPLATE = """Eres un asistente de peliculas. Tu tarea es guiar al usuario para utilizar la película que mejor se ajuste a sus requisitos.
 
 Usa la herramienta de busqueda de herramienta cuando hayas recogido suficiente información. Entonces, responde siguiendo estas reglas:
